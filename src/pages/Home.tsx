589dd9444309f51d453c5e2c6af693f2eca51817
Adwaith Jayasankar <kichu31084@gmail.com>
2024-05-26 00:30:53 +0530

    Update: Added new features to home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,17 @@
-import { Navbar } from "../components/Navbar"
-
+import { Navbar } from '../components/Navbar';
+import Header from '../components/Header';
+import Features from '../components/Features';
+import CallToAction from '../components/CallToAction';
 
 const Home = () => {
   return (
-    <div>
+    <div className="font-sans">
       <Navbar />
+      <Header />
+      <Features />
+      <CallToAction />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
