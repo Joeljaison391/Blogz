ed5990a4dde73188a8acbaeecfa3e60d6aada200
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-05-27 22:21:54 +0530

    refactor: Update .gitignore to exclude .env file and dist-ssr folder

--- src/utils/blobService.ts
+++ src/utils/blobService.ts
@@ -1,13 +1,11 @@
 import { BlobServiceClient } from '@azure/storage-blob';
 
 const account = 'blogz'
-const sasToken = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2026-05-27T16:28:03Z&st=2024-05-27T08:28:03Z&spr=https,http&sig=VIaz7HcdTweAYFr0mC9Y0RnNXvP41RtafwxjGosCTzo%3D"
-// const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net/?${sasToken}`); 
+const sasToken = process.env.REACT_APP_AZURE_STORAGE_SAS_TOKEN// const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net/?${sasToken}`); 
 const containerName = "img";
 
 export const uploadImageToAzure = async (file: File): Promise<string | null> => {
   try {
-    // const blobServiceClient = new BlobServiceClient(`https://blogz.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sc&sp=rwdlacupiytfx&se=2026-05-27T16:28:03Z&st=2024-05-27T08:28:03Z&spr=https,http&sig=4Iqq2pL8N94Gus5ADtrVPWuiyiEeb75BTkxoTibXY6k%3D`);
     const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net/?${sasToken}`); 
     const containerClient = blobServiceClient.getContainerClient(containerName);
     console.log(containerClient)
